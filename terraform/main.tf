b38c779098f06ee8ce0f5964d747d63c2c79a1b5
root <root@ip-172-31-28-99.ec2.internal>
2025-01-06 12:12:01 +0000

    terrafrm file added

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "key" {
+  tags = {
+    Name        = var.iname
+    Environment = "dev"
+  }
+  ami                    = var.ami_id
+  instance_type          = var.itype
+  key_name               = var.key-name
+  count                  = 1
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  root_block_device {
+    volume_size = 12
+  }
+}
---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAU6VTTOI6RQVJGP6E"
+  secret_key = "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"
+}
