d0b92f974ebce6a53d727d7cc2f059765979e23d
sai25052001 <101008605+sai25052001@users.noreply.github.com>
2025-02-15 09:56:37 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAU6VTTOI6RQVJGP6E"
-  secret_key = "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"
+  access_key = "AKIAU6VTTOI62WFUHIXM"
+  secret_key = "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"
 }
