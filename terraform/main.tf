5c76f3400bfedf20af9d9a359044b4427f1b738c
sai25052001 <101008605+sai25052001@users.noreply.github.com>
2025-02-16 08:43:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,9 @@ resource "aws_instance" "key" {
   }
   ami                    = var.ami_id
   instance_type          = var.itype
-  key_name               = var.key-name
+  key_name               = var.key_name
   count                  = 1
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
   root_block_device {
     volume_size = 12
   }
